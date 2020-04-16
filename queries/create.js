b8bedb798ede082a1b402a7994ac5b52172ae08a
Peter Kertesz <kerteszpetr@gmail.com>
2020-04-16 07:11:19 +0100

    Creating user function is working without providing team ID.

--- queries/create.js
+++ queries/create.js
@@ -3,6 +3,7 @@
 // ****************************************************
 const Items = require('../models/items');
 const Categories = require('../models/categories');
+const Users = require('../models/users');
 
 // ****************************************************
 // CREATE A NEW ITEM AND UPDATE CORRESPONDING CATEGORY
@@ -20,6 +21,47 @@ const createItem = function(category_id, category_name, item) {
 		);
 	});
 };
+
+// ****************************************************
+// CREATE A NEW USER AND UPDATE CORRESPONDING TEAM
+// ****************************************************
+const createUser = function(user, team_name = 'undefined', team_id = 'undefined') {
+	return Users.create(user).then((new_user) => {
+		console.log('\n>> Created user:\n', new_user);
+
+		if (team_id !== 'undefined') {
+			console.log('\n>> Team ID:\n', team_id);
+			user['team_name'] = team_name;
+			return Teams.findByIdAndUpdate(
+				team_id,
+				{ $push: { users: new_user._id } },
+				{ new: true, useFindAndModify: false }
+			);
+		}
+	});
+};
+
+// ****************************************************
+// NEW USERS WITH SAMPLE DATA
+// ****************************************************
+const SampleUsers = [
+	{
+		firstname: 'Peter',
+		middlename: '',
+		lastname: 'Kertesz',
+		email: 'kerteszpetr@gmail.com',
+		password: 'password',
+		joined_at: Date.now()
+	},
+	{
+		firstname: 'Nikolett',
+		middlename: 'Alexandra',
+		lastname: 'Nemes',
+		email: 'nyikkanoble@gmail.com',
+		password: 'password',
+		joined_at: Date.now()
+	}
+];
 
 // ****************************************************
 // CREATE A NEW CATEGORY
@@ -103,17 +145,21 @@ const FindCategoryIDByName = function(CategoryName) {
 // RUNNING CODE FOR STORING SAMPLE DATA
 // ****************************************************
 const run = async function() {
-	GenerateCategories();
-	SampleItems.forEach(async (SampleItem) => {
-		let CategoryID = await FindCategoryIDByName(SampleItem.Category);
-		await createItem(CategoryID, SampleItem.Category, SampleItem.Item);
+	// GenerateCategories();
+	// SampleItems.forEach(async (SampleItem) => {
+	// 	let CategoryID = await FindCategoryIDByName(SampleItem.Category);
+	// 	await createItem(CategoryID, SampleItem.Category, SampleItem.Item);
+	// });
+
+	SampleUsers.forEach(async (SampleUser) => {
+		await createUser(SampleUser);
 	});
 };
 
 // ****************************************************
 // RUN THIS STATEMENT ONLY WITH CARE
 // ****************************************************
-// run();
+run();
 
 const Create = {
 	createItem,
